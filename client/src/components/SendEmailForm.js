6f4e7574beab0813a0b6e715ce95b9c4a32d6568
alex-eugene-hunt <my email address used on my git repo>
2025-02-11 09:50:36 -0800

    Send me a message

--- client/src/components/SendEmailForm.js
+++ client/src/components/SendEmailForm.js
@@ -124,7 +124,7 @@ const styles = {
     paddingBottom: '2.5rem',
     fontSize: '0.9rem',
     color: '#434a54',
-    borderRadius: '10px 10px 50px 50px',
+    borderRadius: '10px 10px 40px 40px',
     flex: 1,
     resize: 'none',
     fontFamily: '"Montserrat", sans-serif',
