0d8a50f778ba5eafce49e36d8aaeb49134a46afc
alex-eugene-hunt <my email address used on my git repo>
2025-02-12 13:11:21 -0800

    Homepage tweaking

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ContentSections from './components/ContentSections';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
