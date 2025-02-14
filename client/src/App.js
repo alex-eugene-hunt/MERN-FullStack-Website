837f35206eae52bd09e81eac968721f5d9c8f989
alex-eugene-hunt <my email address used on my git repo>
2025-02-14 04:24:18 -0800

    Background

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import ExperienceSection from './components/ExperienceSection';
 import EducationSection from './components/EducationSection';
 import ProjectsSection from './components/ProjectsSection';
 import SocialSection from './components/SocialSection';
+import '@fontsource/lobster';
 import './styles.css';
+import './App.css';
 
 function App() {
   return (
