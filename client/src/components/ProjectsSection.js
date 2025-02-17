e69b5ae0c40753549080abf5b43bdd2ee0abfbb1
alex-eugene-hunt <my email address used on my git repo>
2025-02-17 12:12:30 -0800

    Project README.md

--- client/src/components/ProjectsSection.js
+++ client/src/components/ProjectsSection.js
@@ -95,7 +95,7 @@ function ProjectsSection() {
 
   return (
     <div style={{ 
-      minHeight: '100vh', 
+      minHeight: 'auto',
       height: '100%',
       backgroundColor: '#021825',
       backgroundImage: 'linear-gradient(135deg, rgba(67, 74, 84, 0.33) 25%, transparent 25%), linear-gradient(225deg, rgba(67, 74, 84, 1) 25%, transparent 25%), linear-gradient(315deg, rgba(67, 74, 84, 0.33) 25%, transparent 25%), linear-gradient(45deg, rgba(67, 74, 84, 1) 25%, #021825 25%)',
@@ -105,7 +105,7 @@ function ProjectsSection() {
       <div className="section-header">Projects</div>
       <section style={{
         padding: '2rem 0 0 0',
-        minHeight: '100vh',
+        minHeight: 'auto',
       }}>
         <div style={{
           maxWidth: '1200px',
